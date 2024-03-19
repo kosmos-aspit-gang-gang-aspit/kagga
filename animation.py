52a2fde505987a91ff0aa01b9e3a6b1d17917be9
65763 <noahsantan22@gmail.com>
2024-03-19 11:25:17 +0100

    fixes

--- animation.py
+++ animation.py
@@ -2,23 +2,29 @@ import pygame
 
 
 class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, images, screen):
+    def __init__(self, idle_sprites, walking_sprites, jumping_sprites, screen):
         super().__init__()
         self.index = 0
         self.animation_speed = 0.2
-        self.images = images
+        self.images = walking_sprites
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = 100
         self.screen = screen
 
-    def update(self):
+    def update(self, flipped):
         self.index += self.animation_speed
         if self.index >= len(self.images):
             self.index = 0
         self.image = self.images[int(self.index)]
         # drawing moved here:
+        self.image = pygame.transform.flip(self.images[int(self.index)], flipped, False)
+        friction_coefficient = 0.7
+        self.vx *= friction_coefficient
+        self.vy *= friction_coefficient
+        self.rect.x += self.vx
+        self.rect.y += self.vy
         self.screen.fill((0, 0, 0))  # clear the screen with black, placeholder for background
         self.screen.blit(self.image, self.rect)  # draw the sprite
         pygame.display.flip()  # update the screen
--- game.py
+++ game.py
@@ -6,33 +6,55 @@ from animation import AnimatedSprite
 class Mario(AnimatedSprite):
     def __init__(self, screen):
         # load mario sprites
-        sprites = []
-        _dir = "sprites/mario"
+        idle_sprites = []
+        walking_sprites = []
+        jumping_sprites = []
+        _dir = "sprites/mario/idle_sprites"
         for filename in os.listdir(_dir):
             filepath = os.path.join(_dir, filename)
             sprite = pygame.image.load(filepath).convert_alpha()  # alpha conversion optimizes performance
-            sprites.append(sprite)
+            idle_sprites.append(sprite)
+        _dir = "sprites/mario/walking_sprites"
+        for filename in os.listdir(_dir):
+            filepath = os.path.join(_dir, filename)
+            sprite = pygame.image.load(filepath).convert_alpha()  # alpha conversion optimizes performance
+            walking_sprites.append(sprite)
+        _dir = "sprites/mario/jumping_sprites"
+        for filename in os.listdir(_dir):
+            filepath = os.path.join(_dir, filename)
+            sprite = pygame.image.load(filepath).convert_alpha()  # alpha conversion optimizes performance
+            jumping_sprites.append(sprite)
 
         # init animation
-        super().__init__(sprites, screen)
+        super().__init__(idle_sprites=idle_sprites, walking_sprites=walking_sprites, jumping_sprites=jumping_sprites, screen=screen)
 
+        # player movement
         self.speed = 10
+        self.jump_power = self.speed * 3
+        self.vx = 0  # velocity x-axis
+        self.vy = 0  # velocity y-axis
+
+        # player state
         self.moving = False
         self.flipped = False
         self.airborne = False
         self.crouched = False
 
     def move_up(self):
-        self.rect.y -= self.speed
+        self.vy -= self.jump_power
+        self.airborne = True  # this needs to be reset somewhere
 
     def move_left(self):
-        self.rect.x -= self.speed
+        self.vx -= self.speed
+        self.flipped = True
 
     def move_down(self):
-        self.rect.y += self.speed
+        self.crouched = True
+        # couch
 
     def move_right(self):
-        self.rect.x += self.speed
+        self.vx += self.speed
+        self.flipped = False
 
 
 class Game:
@@ -54,22 +76,21 @@ class Game:
                     running = False
 
             keys = pygame.key.get_pressed()  # pygame has a built in keyboard function with better performance
-            if keys[pygame.K_w]:
+            if keys[pygame.K_w] and not self.mario.airborne:
                 self.mario.move_up()
             if keys[pygame.K_a]:
                 self.mario.move_left()
-                self.mario.moving = True
-                self.mario.flipped = True
             if keys[pygame.K_s]:
                 self.mario.move_down()
-                self.mario.crouched = True
             if keys[pygame.K_d]:
                 self.mario.move_right()
-                self.mario.moving = True
-                self.mario.flipped = False
 
-            self.mario.update()
-            self.mario.moving = False
+            if not (keys[pygame.K_a] or keys[pygame.K_d]):
+                self.mario.moving = False
+            else:
+                self.mario.moving = True
+
+            self.mario.update(flipped=self.mario.flipped)
             self.mario.crouched = False
             clock.tick(24)  # FPS cap
 
